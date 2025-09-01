5ce91b2671166a5e156e0358111cedaa7d0f350c
Liathakral <132537723+Liathakral@users.noreply.github.com>
2025-09-01 09:54:43 +0530

    llama

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ groq_api_key=os.getenv("GROQ_API_KEY")
 os.environ['HF_TOKEN']=os.getenv("HF_TOKEN")
 embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
-llm=ChatGroq(groq_api_key=groq_api_key,model_name="llama3-70b-8192")
+llm=ChatGroq(groq_api_key=groq_api_key,model_name="llama-3.1-8b-instant")
 
 prompt=ChatPromptTemplate.from_template(
     """
